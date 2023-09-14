22f63ad8a724770e2b22641fe766de1863819f79
Akash Rawat <Akash473@users.noreply.huggingface.co>
2023-09-14 11:15:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -243,7 +243,8 @@ background_images = [gr.inputs.Image(type="pil", label="Background Image " + str
 gr.Interface(
     fn=generate_funko_figurines,
     inputs=input_image,
-    outputs=[gr.outputs.Image(type="numpy", label="Generated Image " + str(i + 1)) for i in range(3)],
+    #outputs=[gr.outputs.Image(type="numpy", label="Generated Image " + str(i + 1)) for i in range(3)],
+    outputs=["image", "image", "image"]
     title="Funko Figurine Generator",
     description="Generate personalized Funko figurines with different styles and backgrounds.",
 ).launch()
